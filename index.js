a72f810aa776d86ccf995be1147bcb9d5bf01b42
Allison Howard <alliemhow@gmail.com>
2019-06-26 22:48:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function driversWithRevenueOver (drivers, revenue) {
 }
 
 function driverNamesWithRevenueOver (drivers, revenue) {
-  return driversWithRevenueOver
+  return driversWithRevenueOver (drivers, revenue)
 }
